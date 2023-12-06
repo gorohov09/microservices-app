23610f64e143d1ad001e141c7ae1761869719882
AndrewGorohov <andrej.gorokhoff2017@yandex.ru>
2023-12-06 22:29:51 +0300

    Реализация отправления сообщения о смене цены на товар

---
+++ src/Services/Catalog/Catalog.RabbitMQ/Entry.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Core.Contracts.Messages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+
+namespace Catalog.RabbitMQ
+{
+    /// <summary>
+	/// Класс - входная точка проекта, регистрирующий реализованные зависимости текущим проектом
+	/// </summary>
+	public static class Entry
+    {
+        /// <summary>
+        /// Добавить службы проекта с очередью
+        /// </summary>
+        /// <param name="services">Коллекция служб</param>
+        /// <param name="connectionString">Строка подключения к RMQ</param>
+        /// <returns>Обновленная коллекция служб</returns>
+        public static IServiceCollection AddRabbitMq(this IServiceCollection services, IConfiguration configuration)
+        {
+            var hostAddress = configuration["EventBusSettings:HostAddress"];
+            var productPriceUpdatedExchangeName = configuration["EventBusSettings:Producers:PriceUpdated:ExchangeName"];
+
+            services.AddMassTransit(hostAddress, options => options
+                .AddProducer<ProductPriceUpdatedIntegrationEvent>(exchangeName: productPriceUpdatedExchangeName));
+
+            return services;
+        }
+    }
+}
