e4c07891b6a112278a3ec1bee5365fd9aee8f3e7
AndrewGorohov <andrej.gorokhoff2017@yandex.ru>
2023-12-10 18:55:21 +0300

    Подобие паттерна - агрегатор + закинул его под шлюз

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Services;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public ShoppingController(IBasketService basketService)
+        {
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+        }
+
+        [HttpGet("{userName}", Name = "GetShopping")]
+        [ProducesResponseType(typeof(BasketModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketModel>> GetShopping(string userName)
+        {
+            var basket = await _basketService.GetBasket(userName);
+
+            return Ok(basket);
+        }
+    }
+}
